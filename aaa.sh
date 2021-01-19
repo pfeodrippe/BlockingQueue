c218a69b7aee9f6b7cd1769c76916a5db3645733
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2021-01-19 01:16:22 -0300

    ggg

--- aaa.sh
+++ aaa.sh
@@ -1,8 +1,15 @@
 java -cp /Users/paulo.feodrippe/dev/tlaplus/tlatools/org.lamport.tlatools/dist/tla2tools.jar tlc2.TraceExplorer -overwrite -traceExpressions -expressionsFile eita.out BlockingQueue ; \
     cat TE.tla TTrace.tla > Eita.tla && \
         mv Eita.tla TE.tla && \
-        sed -i '/^EXTENDS.*/a\
+        sed -i"..." '/^EXTENDS.*/a\
  CONSTANTS p1, p2, p3, p4' TE.tla && \
-        rm TTrace.tla && \
-        rm TTrace.cfg && \
-        java -cp /Users/paulo.feodrippe/dev/tlaplus/tlatools/org.lamport.tlatools/dist/tla2tools.jar:CommunityModules.jar -XX:+UseParallelGC tlc2.TLC -noGenerateSpecTE -tool TE.tla -config TE.cfg
+        sed '/^CONSTANTS/r'<(
+    echo "p1 = p1"
+    echo "p2 = p2"
+    echo "p3 = p3"
+    echo "p4 = p4"
+) TE.cfg > /tmp/TE.cfg && \
+            cp /tmp/TE.cfg TE.cfg && \
+            rm TTrace.tla && \
+            rm TTrace.cfg && \
+            java -cp /Users/paulo.feodrippe/dev/tlaplus/tlatools/org.lamport.tlatools/dist/tla2tools.jar:CommunityModules.jar -XX:+UseParallelGC tlc2.TLC -noGenerateSpecTE -tool TE.tla -config TE.cfg
